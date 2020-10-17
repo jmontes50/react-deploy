0be9e320e9453345eed1a5da695a605e0fef4d9e
user <user@itoour.com>
2020-10-16 21:56:20 -0500

    sdf

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Codigo Online - You're awesome
         </a>
       </header>
     </div>
